97819d1677f124b903d00d6ad068a6dcb379c6ea
Eluriomilmar <lucasrfranca@hotmail.com>
2023-10-28 22:01:01 -0300

    Teste

--- 5.3.1.1.py
+++ 5.3.1.1.py
@@ -1,1 +1,18 @@
-#Recursiva
+#Função iterativa para calcular Phi
+
+import math
+
+Phi = float(0)
+Loops = int(input("Deseja calcular Phi em quantas iterações? "))
+print("O cálculo de Phi em %i iterações é igual a: " % Loops, end = (""))
+
+while Loops > 0:
+    if Phi == 0:
+        Phi = math.sqrt(1)
+        Loops = Loops -1
+    else:
+        Phi = math.sqrt(Phi+math.sqrt(1))
+        Loops = Loops - 1
+
+print("%f" % Phi)
+
--- 5.3.1.2.py
+++ 5.3.1.2.py
@@ -1,1 +1,15 @@
-#Iterativa
+#Recursiva
+import math
+
+Iteracoes = int(input("Em quantas iterações deseja calcular Phi? "))
+
+def phiCalc(i):
+    if i == 1:
+        return 1
+    else:
+        return math.sqrt(1 + math.sqrt(phiCalc(i - 1)))
+
+Resultado = phiCalc(Iteracoes)
+
+if Iteracoes >= 1:
+    print ("O valor de Phi em %i iterações é igual a: %f" % (Iteracoes, Resultado))

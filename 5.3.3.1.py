4d08fc94f7174c842cffb6e8761dc671cef0ce93
Eluriomilmar <lucasrfranca@hotmail.com>
2023-10-30 12:14:25 -0300

    Exercícios UFMG, Introdução a Programação de Computadores, 2020, 5° slide.

--- 5.3.3.1.py
+++ 5.3.3.1.py
@@ -1,10 +1,12 @@
-#Função recursiva da forma  somatório(n, k=0) de (1/k!) para n -> inf
+#Função recursiva da forma  somatório(n, k=0) de (1/k!) para n -> inf, #porém trunquei o n em 10
 
 import math
 
-n = math.inf
+#n = math.inf seria o caso ideal, porém a recursão só se permite ser feita 1000 vezes, por isso vou fixar n = k, para que sejam feitas 500 iterações de somatorio(n)
+
+
 k = int(input("Insira a quantidade de iterações do somatório: "))
-print("O valor da função com fatorial de %i iterações é de: " % k, end=(""))
+print("O valor da função somatório com fatorial de %i iterações é de: " % k, end=("")) #Isso é exibido no fim do programa, como a variável "k" é reutilizada, comecei o print aqui
 
 def fatorial(k):
     if k == 0:
@@ -12,14 +14,15 @@ def fatorial(k):
     if k == 1:
         return 1
     elif k > 1:
-        return k * fatorial(k-1)
+        return (1/k) * fatorial(k-1)
 
-def somatorio(k):
-    if k == 0:
-        return 1/fatorial(0)
-    if k == 1:
-        return 1/fatorial(1) + fatorial(0)
-    elif k > 1:
-        return 1/fatorial(k) + 1/fatorial(k-1)
+def somatorio(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n > 1:
+        return fatorial(n) + somatorio(n - 1) #para o somatório de 3, pro exemplo, a função faz 1/3! + 1/2! + 1/1!
 
 print("%f" % somatorio(k))
+print("\nFatorial: %f" % fatorial(k))

1ac68656b680d96c5bdd4769b54814931cafcc56
Eluriomilmar <lucasrfranca@hotmail.com>
2023-11-05 10:33:34 -0300

    Exercícios UFMG, Introdução a Programação de Computadores, 2020, 5° slide.

---
+++ 6.3.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        palindromo = str(input("Insira string a ser verificada se é palíndromo: "))
+        break
+    except ValueError:
+        print("Alguma coisa deu errado, oxe, achei que isso seria impossível aqui, só escrevi esse código pra fixar na minha cabeça socorro")
+
+def pali_check(palavra):
+    i = 0
+    while i < len(palavra):
+        if (palavra[i:i+1:]) != (palavra[-i-1:-i-2:-1]): #As operações verificam caractere por caractere em sentidos opostos
+            print("A palavra %s não é palíndromo" % palavra)
+            break
+        elif i >= len(palavra)/2:
+                print("A palavra %s é palíndromo" % palavra)
+                break
+        i = i + 1
+
+pali_check(palindromo)
+
+#também é possível fazer o mesmo exercício usando somente operações de string, definindo len(palavra)
+#e comparando palavra[0:len(palavra)/2-1:0] com palavra[len(palavra)*-1:len(palavra)*-1:-1] ou operação similar

61e25d545e03035c6952e9a52c3427e8cfd012d5
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-06-18 20:16:21 -0300

    Curso em Vídeo, Python.

--- Curso Python #21.py
+++ Curso Python #21.py
@@ -1,6 +1,2 @@
-import pygame
-pygame.init()
-file1 = open("E:\\Backup\\Desktop\\244650023_263457539005560_6438936383667695999_n.mp4", "r")
-pygame.mixer.music.load(file1)
-pygame.mixer.music.play()
-pygame.event.wait()
+file = open("E:\\Backup\\Desktop\\troço.txt")
+print(file.read())

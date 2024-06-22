3187152154f4978a774a2b254a45f3ff58a67a06
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-06-22 00:38:05 -0300

    Curso em Vídeo, Python.

---
+++ Curso Python #075.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+#No final, mostre:
+#A) Quantas vezes apareceu o valor 9.
+#B) Em que posição foi digitado o primeiro valor 3.
+#C) QUais foram os números pares.
+tupla = (int(input("Insira o primeiro valor: ")), int(input("Insira o segundo valor: ")), int(input("Insira o terceiro valor: ")), int(input("Insira o quarto valor: ")))

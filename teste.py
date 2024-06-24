0969e863f2c9282b58047d1869d952616141cfc7
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-06-24 02:40:33 -0300

    Curso em Vídeo, Python.

--- teste.py
+++ teste.py
@@ -1,2 +1,8 @@
-#arquivos somente para testes
-print("\033[30;45mcoisa feia\033[m")
+#arquivo somente para testes
+valores = []
+for i in range(10):
+    valores.append(int(input("Insira valor para ser adicionado à lista: ")))
+valores.sort(reverse=True)
+
+for i, v in enumerate(valores):
+    print(f"Na posição {i}, encontrei o valor {v}")

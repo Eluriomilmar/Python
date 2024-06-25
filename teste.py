5dc432d5014d8b63f2389ec0c486cfe682194de7
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-06-25 20:20:32 -0300

    Curso em Vídeo, Python.

--- teste.py
+++ teste.py
@@ -1,2 +1,13 @@
-#arquivo somente para testes
-print(range(10))
+cont = True
+composta = list()
+while cont == True:
+    base = list()
+    base.append(int(input("Insira número inteiro: ")))
+    base.append(int(input("Insira outro número inteiro: ")))
+    composta.append(base[:])
+    base.clear()
+    if str(input("Deseja continuar? (S/N): ")).upper() == "S":
+        cont = True
+    else:
+        cont = False
+print(composta)

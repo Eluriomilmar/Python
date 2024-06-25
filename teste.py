4fdf88eca9cb7ce3af937222c8e01895f16c4d0b
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-06-24 21:47:23 -0300

    Curso em Vídeo, Python.

--- teste.py
+++ teste.py
@@ -1,8 +1,2 @@
 #arquivo somente para testes
-valores = []
-for i in range(10):
-    valores.append(int(input("Insira valor para ser adicionado à lista: ")))
-valores.sort(reverse=True)
-
-for i, v in enumerate(valores):
-    print(f"Na posição {i}, encontrei o valor {v}")
+print(range(10))

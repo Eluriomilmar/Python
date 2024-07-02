f028b962fe19d1f60ce4bb6569fdf409d08a5f13
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-02 19:57:51 -0300

    Curso em Vídeo, Python.

--- Curso Python #015.py
+++ Curso Python #015.py
@@ -1,4 +1,9 @@
 #Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado
-distancia = float(input("Insira a quantidade de Km percorridos pelo carro: "))
-dias = int(input("Insira a quantidade de dias pelos quais o carro foi alugado: "))
-print(f"O valor a ser pago é igual a {distancia * 0.15 + dias * 60:.2f}")
+try:
+    n = int(input("digite um n: "))
+except:
+    n = int(input("O erro encontrado foi {erro.__class__}\nInsira número inteiro válido: "))
+else:
+    print(f"Você digitou {n}")
+finally:
+    print("Programa encerrado.")
--- teste.py
+++ teste.py
@@ -1,6 +1,11 @@
 def fatorial(n):
     if n == 1:
         return 1
-    return n*(fatorial(n-1))
+    else:
+        return n * (fatorial(n-1))
 
-print(fatorial(5))
+
+
+n = fatorial(5)
+
+print(f"o dobro do número é igual a {n}")

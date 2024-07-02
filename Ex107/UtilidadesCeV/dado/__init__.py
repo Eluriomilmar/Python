df2fcf5f3e212759ec11fedab21bf89bacbc1539
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-02 14:12:09 -0300

    Curso em Vídeo, Python.

---
+++ Ex107/UtilidadesCeV/dado/__init__.py
@@ -0,0 +1,7 @@
+def leiaDinheiro(n):
+    if n.isdigit() == True:
+        return float(n)
+    else:
+        while n != isdigit():
+            print("Insira dígito válido como moeda: ")
+            n = str(input(""), end = "")

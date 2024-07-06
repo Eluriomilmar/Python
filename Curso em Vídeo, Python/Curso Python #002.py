f9b8c455b4163e8749021ad05f25afdff9dba438
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-06 16:07:23 -0300

    Practice Python exercises.

--- Curso em Vídeo, Python/Curso Python #002.py
+++ Curso em Vídeo, Python/Curso Python #002.py
@@ -1,10 +1,10 @@
 #Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uam mensagem com a data formatada.
 import datetime
-hoje = datetime.datetime.now()
-hoje = int(hoje.toordinal())
+hoje_ano = datetime.datetime.now().year
+hoje_mes = datetime.datetime.now().month
+hoje_dia = datetime.datetime.now().day
 dia = int(input("Insira o dia de nascimento do usuário: "))
 mes = int(input("Insira o mês de nascimento do usuário: "))
 ano = int(input("Insira o ano de nascimento do usuário: "))
-conta = hoje - (ano*365.25 + mes*30.416 + dia)
-conta = conta/365
+conta = hoje_ano + hoje_mes/12 + hoje_dia/30 - ano - mes/12 - dia /30
 print("O usuário tem aproximadamente %.0f anos" % conta)
---
+++ Practice Python/Exercise 16.py
@@ -0,0 +1,35 @@
+from random import choice
+from random import randint
+
+def passlen():
+    while True:
+        try:
+            len = int(input("Insira o tamanho desejado para a senha: "))
+            if len < 1:
+                raise ValueError
+            dif = int(input("Insira a dificuldade desejada para a senha(1-3): "))
+            if dif < 1 or dif > 3:
+                raise ValueError
+        except:
+            print("Insira somente números inteiros!")
+        else:
+            return len, dif
+
+
+def password(length, difficulty):
+    vetor1 = ["A", "B", "C", "D", "E", "F", "G"]
+    vetor2 = ["a", "b", "c", "d", "e", "f", "g"]
+    vetor3 = ["!", "@", "#", "$", "%", "&", "*"]
+    vetorTodos = [vetor1, vetor2, vetor3]
+    vetorFinal = []
+    a = 0
+    while a < length:
+        vetorFinal.append(choice(vetorTodos[randint(0, difficulty-1)]))
+        a += 1
+    vetorFinal = "".join(vetorFinal)
+    return vetorFinal
+
+
+len, dif = passlen()
+print(f"A senha gerada é: {password(len, dif)}")
+

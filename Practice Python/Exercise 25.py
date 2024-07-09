6fe8df9c75f74d7be8e2b99755c257de96bda31d
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-08 23:48:45 -0300

    Practice Python exercises.

---
+++ Practice Python/Exercise 25.py
@@ -0,0 +1,48 @@
+from random import randint
+
+
+def palpite():
+    return randint(0, 100)
+
+
+def cria_lista():
+    lista = []
+    for i in range(101):
+        lista.append(i)
+    return lista
+
+def valida_palpite():
+    while True:
+        try:
+            a = str(input(f"O número é maior, menor ou igual a {numero}? ")).upper()
+            if a != "MAIOR" and a!= "MENOR" and a != "IGUAL":
+                raise ValueError
+        except:
+            print("Valores válidos são Maior, Menor e Igual.")
+        else:
+            return a
+
+possibilidades = cria_lista()
+tentativas = 0
+global numero
+anterior = -1
+while True:
+    try:
+        numero = possibilidades[len(possibilidades)//2]
+        if anterior == numero:
+            raise IndexError
+        palpite = valida_palpite()
+        anterior = numero
+        if palpite == "MAIOR":
+            possibilidades = possibilidades[len(possibilidades)//2:]
+            tentativas += 1
+        elif palpite == "MENOR":
+            possibilidades = possibilidades[:len(possibilidades)//2]
+            tentativas += 1
+        else:
+            tentativas += 1
+            print(f"O número que você pensou é {numero}\nForam feitas {tentativas} tentativas até acertar.")
+            break
+    except IndexError:
+        print("Você está trapaceando! Encerrando o programa.")
+        break

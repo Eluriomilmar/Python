f74fadd3d5b2ae4015efd0059b8cf4eae32f2e4c
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-18 16:45:42 -0300

    Curso em Vídeo, Python

--- Curso em Vídeo, Python/Curso Python #022.py
+++ Curso em Vídeo, Python/Curso Python #022.py
@@ -1,4 +1,5 @@
-#Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas, o nome com todas as letras minúsculas, quantas letras ao todo(s/ espaços) e quantas letras tem o primeiro nome
+"""Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas, o nome com todas as letras minúsculas,
+quantas letras ao todo(s/ espaços) e quantas letras tem o primeiro nome"""
 nome = input("Insira o nome completo: ")
 print(f"Nome maiúsculo: {nome.upper()}")
 print(f"Nome minúsculo: {nome.lower()}")

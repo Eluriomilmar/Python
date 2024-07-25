661df1370f538234932c60f7f2da64328f3df281
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-07-25 17:05:05 -0300

    Projetos pessoais

---
+++ Pessoais/Remove acentuação .txt.py
@@ -0,0 +1,19 @@
+import re
+
+
+with open("soletrando.txt") as arquivo:
+    with open("soletrando2.txt", "w") as arquivo2:
+        for tudo in arquivo:
+            tudo = tudo.lstrip()
+            tudo = tudo.rstrip()
+            tudo = tudo.replace(";", "")
+            tudo = tudo.replace(".", "")
+            for letras in tudo:
+                if letras in "áàãâéèêíìóúçÁÀÃÊÉÈÊÍÌÓÚÇ":
+                    tudo = ""
+            if tudo == "":
+                continue
+            arquivo2.write(tudo)
+            arquivo2.write("\n")
+        arquivo2.close()
+    arquivo.close()
---
+++ Pessoais/Soletrando.py
@@ -0,0 +1,72 @@
+from random import choice
+import re
+
+
+def palpite(palavra, tentativa=" ", erros=[], acertos=[]):
+    if tentativa == " ":
+        for i in range(len(palavra)):
+            print("_", end="")
+    tentativa = str(input("\nInsira letra da tentativa: ")).upper()
+    while len(tentativa) > 1 or bool(re.search("[A-Z]", tentativa)) is False:
+        try:
+            tentativa = str(input("\nInsira letra de A a Z: ")).upper()
+            if len(tentativa) != 1 or bool(re.search("[A-Z]", tentativa)) is False:
+                raise ValueError
+        except:
+            print("\nInsira somente um caractere alfabético.")
+    sequencia = 0
+    if tentativa in palavra:
+        acertos.append(tentativa)
+        for i in palavra:
+            if i in tentativa or i in acertos:
+                print(i, end="")
+                sequencia += 1
+            else:
+                print("_", end="")
+    else:
+        erros.append(tentativa)
+        for i in palavra:
+            if i in tentativa or i in acertos:
+                print(i, end="")
+                sequencia += 1
+            else:
+                print("_", end="")
+    print(f"\nErros: {erros}")
+    if len(erros) == 0:
+        print("o")
+    elif len(erros) == 1:
+        print(" o")
+        print(" |")
+    elif len(erros) == 2:
+        print(" o")
+        print(" |")
+        print("/")
+    elif len(erros) == 3:
+        print(" o")
+        print(" |")
+        print("/ \\ ")
+    elif len(erros) == 4:
+        print(" o")
+        print("/|")
+        print("/ \\ ")
+    if len(erros) == 5:
+        print(" o")
+        print("/|\\ ")
+        print("/ \\ ")
+    if len(erros) == 6:
+        print(" x")
+        print("/|\\ ")
+        print("/ \\ ")
+        return print(f"Perdeu! Encerrando programa. A palavra era {palavra}")
+    if len(palavra) == sequencia:
+        return print(f"Venceu! A palavra é {palavra}.\n\nEncerrando programa")
+    return palpite(palavra, tentativa, erros, acertos)
+
+
+def cria_lista(arq):
+    with open(arq, "r") as dicionario:
+        vetor = dicionario.read().upper().split("\n")
+        return choice(vetor)
+
+
+palpite(cria_lista("soletrando2.txt"), " ", [], [])

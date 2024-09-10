c35589346c54a70fe6a66083dd636e8e7069a140
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-09-10 18:49:17 -0300

    Pessoais

---
+++ Pessoais/Teste.py
@@ -0,0 +1,5 @@
+linhas_vistas = set()
+with open("Frases Desmotivacionais.txt", "r") as arq1:
+    with open("Frases Desmotivacionais.txt", "w") as arq2:
+        linhas = arq1.readlines()
+        print(linhas)

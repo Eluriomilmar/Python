2f523c7505cd4eb44298d496f527612980ec5dba
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-09-12 12:00:34 -0300

    Pessoais

--- Pessoais/Escreve Lag.py
+++ Pessoais/Escreve Lag.py
@@ -4,20 +4,28 @@ from datetime import datetime
 
 arq = str(input("Insira mês de registro: "))
 with (open(arq + ".txt", "a") as arquivo):
-    a = input("Insira a tecla de registro: ")
+    a = input("Insira a tecla de registro de lag: ")
+    b = input("Insira a tecla de registro de queda: ")
     c = input("Insire a tecla de pausa: ")
-    b = input("Insira a tecla de parada: ")
+    d = input("Insira a tecla de parada: ")
     while True:
-        d = keyboard.read_key()
-        if d == a:
+        e = keyboard.read_key()
+        if e == a:
             arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+"\n")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" - Lag\n")
             print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m - Lag")
             arquivo.flush()
             sleep(0.5)
-        if d == b:
+        if e == b:
+            arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" - Queda\n")
+            print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m - Queda")
+            arquivo.flush()
+            sleep(0.5)
+        if e == c:
             break
-        if d == c:
+        if e == d:
             input("Programa pausado, aperte enter para continuar.")
             print("Programa reinicializado.")

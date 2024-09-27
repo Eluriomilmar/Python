6c090ebcf3d8ea0f666dc917abe1bbb1f2723cfc
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-09-27 20:47:28 -0300

    Pessoais

--- Pessoais/Escreve Lag.py
+++ Pessoais/Escreve Lag.py
@@ -11,17 +11,39 @@ with (open(arq + ".txt", "a") as arquivo):
     while True:
         e = keyboard.read_key()
         if e == a:
+            var = 1
+            if len(str(datetime.today().day)) < 2:
+                var += 1
+            if len(str(datetime.today().month)) < 2:
+                var += 1
+            if len(str(datetime.today().day)) < 2:
+                var += 1
+            if len(str(datetime.today().hour)) < 2:
+                var += 1
+            if len(str(datetime.today().minute)) < 2:
+                var += 1
             arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" - Lag\n")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Lag\n")
             print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m - Lag")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Lag")
             arquivo.flush()
             sleep(0.5)
         if e == b:
+            var = 1
+            if len(str(datetime.today().day)) < 2:
+                var += 1
+            if len(str(datetime.today().month)) < 2:
+                var += 1
+            if len(str(datetime.today().day)) < 2:
+                var += 1
+            if len(str(datetime.today().hour)) < 2:
+                var += 1
+            if len(str(datetime.today().minute)) < 2:
+                var += 1
             arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" - Queda\n")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Queda\n")
             print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m - Queda")
+            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Queda")
             arquivo.flush()
             sleep(0.5)
         if e == c:

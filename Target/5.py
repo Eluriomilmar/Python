6baf42d857976b167fbae21b6d1190fe1807b81e
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-10-08 03:51:17 -0300

    Pessoais

--- Target/5.py
+++ Target/5.py
@@ -1,3 +1,11 @@
 trem = input("Insira input a ser invertido: ")
-invertida = trem[::-1]
+invertida = []
+for i in range(len(trem)):
+    invertida.append(trem[len(trem)-i-1])
+
+invertida = "".join(invertida)
 print(invertida)
+
+#alternativamente eu poderia só ter feito invertida = trem[::-1]
+
+

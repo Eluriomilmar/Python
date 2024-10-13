783588c3b4ccffe9e72c1ec3f7e5b0936c5bc2e0
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-10-13 20:44:33 -0300

    Pessoais

--- Pessoais/Escreve Lag.py
+++ Pessoais/Escreve Lag.py
@@ -10,40 +10,39 @@ with (open(arq + ".txt", "a") as arquivo):
     d = input("Insira a tecla de parada: ")
     while True:
         e = keyboard.read_key()
+        dia = 0
+        mes = 0
+        hora = 0
+        minuto = 0
         if e == a:
-            var = 1
             if len(str(datetime.today().day)) < 2:
-                var += 1
+                dia = 1
             if len(str(datetime.today().month)) < 2:
-                var += 1
-            if len(str(datetime.today().day)) < 2:
-                var += 1
+                mes = 1
             if len(str(datetime.today().hour)) < 2:
-                var += 1
+                hora = 1
             if len(str(datetime.today().minute)) < 2:
-                var += 1
-            arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Lag\n")
-            print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Lag")
+                minuto = 1
+            arquivo.write(str(datetime.today().day)+" "*dia+"-"+str(datetime.today().month)+" "*mes+"-"+str(datetime.today().year)+
+                          ": "+str(datetime.today().hour)+" "*hora+"h"+str(datetime.today().minute)+" "*minuto+"m"+" - Lag\n")
+            print(str(datetime.today().day)+" "*dia+"-"+str(datetime.today().month)+" "*mes+"-"+str(datetime.today().year)+
+                          ": "+str(datetime.today().hour)+" "*hora+"h"+str(datetime.today().minute)+" "*minuto+"m"+" - Lag")
             arquivo.flush()
             sleep(0.5)
         if e == b:
             var = 1
             if len(str(datetime.today().day)) < 2:
-                var += 1
+                dia = 1
             if len(str(datetime.today().month)) < 2:
-                var += 1
-            if len(str(datetime.today().day)) < 2:
-                var += 1
+                mes = 1
             if len(str(datetime.today().hour)) < 2:
-                var += 1
+                hora = 1
             if len(str(datetime.today().minute)) < 2:
-                var += 1
-            arquivo.write(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Queda\n")
-            print(str(datetime.today().day)+"-"+str(datetime.today().month)+"-"+
-            str(datetime.today().year)+": "+str(datetime.today().hour)+"h"+str(datetime.today().minute)+"m"+" "*var +"- Queda")
+                minuto = 1
+            arquivo.write(str(datetime.today().day)+" "*dia+"-"+str(datetime.today().month)+" "*mes+"-"+str(datetime.today().year)+
+                          ": "+str(datetime.today().hour)+" "*hora+"h"+str(datetime.today().minute)+" "*minuto+"m"+" - Queda\n")
+            print(str(datetime.today().day)+" "*dia+"-"+str(datetime.today().month)+" "*mes+"-"+str(datetime.today().year)+
+                          ": "+str(datetime.today().hour)+" "*hora+"h"+str(datetime.today().minute)+" "*minuto+"m"+" - Queda")
             arquivo.flush()
             sleep(0.5)
         if e == c:

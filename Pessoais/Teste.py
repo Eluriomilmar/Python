d59d11120e0800972d42d561bed9cb0010126a58
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-10-22 00:33:51 -0300

    Pessoais

--- Pessoais/Teste.py
+++ Pessoais/Teste.py
@@ -1,5 +1,24 @@
-linhas_vistas = set()
-with open("Frases Desmotivacionais.txt", "r") as arq1:
-    with open("Frases Desmotivacionais.txt", "w") as arq2:
-        linhas = arq1.readlines()
-        print(linhas)
+if __name__ == '__main__':
+    arr = list()
+    N = int(input())
+    for i in range(N):
+        cmd = input().split()
+        for j in range(len(cmd)):
+            if j > 0:
+                cmd[j] = int(cmd[j])
+        if cmd[0] == "insert":
+            arr.insert(cmd[1], cmd[2])
+        elif cmd[0] == "print":
+            print(arr)
+        elif cmd[0] == "remove":
+            arr.remove(cmd[1])
+        elif cmd[0] == "append":
+            arr.append(cmd[1])
+        elif cmd[0] == "sort":
+            arr.sort()
+        elif cmd[0] == "pop":
+            arr.pop()
+        elif cmd[0] == "reverse":
+            arr.sort(reverse=True)
+        print(arr)
+

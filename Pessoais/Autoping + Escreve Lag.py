50b83462a9c29feb538f864c222ecdca0636f0cd
Eluriomilmar <Lucasrfranca@hotmail.com>
2024-12-27 16:06:00 -0300

    Pessoais

--- Pessoais/Autoping + Escreve Lag.py
+++ Pessoais/Autoping + Escreve Lag.py
@@ -1,13 +1,14 @@
-from pythonping import ping
 from datetime import datetime
 from time import sleep
+import re
+import subprocess
 
 mes = str(input("Insira mês de registro: "))
 ano = str(input("Insira ano de registro: "))
 tolerancia = int(input("Insira valor numérico para a tolerância de lag, em ms: "))
 print(f"Tolerância de lag de {tolerancia}ms")
 with (open(mes + " de " + ano + ".txt", "a") as arquivo):
-    vetor = [0, 0, 0, 0]
+    vetor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     indice = 0
     preenchido = False
     while True:
@@ -16,9 +17,11 @@ with (open(mes + " de " + ano + ".txt", "a") as arquivo):
             mes = 0
             hora = 0
             minuto = 0
-            a = ping("google.com", timeout=1, count=1, interval=1)
-            if a.success == False:
-                raise Exception
+            a = subprocess.check_output("ping google.com -n 1", shell=True)
+            a = str(a)
+            a = (re.search(r"Average = (\d+)", a, re.MULTILINE).group(1))
+            a = float(a)
+            sleep(1)
         except:
             if len(str(datetime.today().day)) < 2:
                 dia = 1
@@ -37,8 +40,9 @@ with (open(mes + " de " + ano + ".txt", "a") as arquivo):
             preenchido = False
             sleep(60)
         if preenchido:
-            media = (vetor[0] + vetor[1] + vetor[2] + vetor[3])/4
-            if a.rtt_avg_ms > media + tolerancia:
+            media = (vetor[0] + vetor[1] + vetor[2] + vetor[3] + vetor[4] +
+                     vetor[5] + vetor[6] + vetor[7] + vetor[8] + vetor[9])/10
+            if a > media + tolerancia:
                 if len(str(datetime.today().day)) < 2:
                     dia = 1
                 if len(str(datetime.today().month)) < 2:
@@ -51,18 +55,18 @@ with (open(mes + " de " + ano + ".txt", "a") as arquivo):
                     " " * dia + str(datetime.today().day) + "-" + " " * mes + str(datetime.today().month) + "-" + str(
                         datetime.today().year) +
                     ": " + " " * hora + str(datetime.today().hour) + "h" + " " * minuto + str(
-                        datetime.today().minute) + "m" + " - Lag de " + str(a.rtt_avg_ms) + "ms, " + (str(a.rtt_avg_ms - (tolerancia + media)))[:6:]
+                        datetime.today().minute) + "m" + " - Lag de " + str(a) + "ms, " + (str(a - (tolerancia + media)))
                       + "ms acima da tolerância.\n")
                 print(" " * dia + str(datetime.today().day) + "-" + " " * mes + str(datetime.today().month) + "-" + str(
                     datetime.today().year) +
                         ": " + " " * hora + str(datetime.today().hour) + "h" + " " * minuto + str(
-                    datetime.today().minute) + "m" + " - Lag de " + str(a.rtt_avg_ms) + "ms, " + (str(a.rtt_avg_ms - (tolerancia + media)))[:6:]
+                    datetime.today().minute) + "m" + " - Lag de " + str(a) + "ms, " + (str(a - (tolerancia + media)))
                       + "ms acima da tolerância.")
                 arquivo.flush()
-        if indice == 3:
+        if indice == 9:
             preenchido = True
-            vetor[indice] = a.rtt_avg_ms
+            vetor[indice] = a
             indice = 0
         else:
-            vetor[indice] = a.rtt_avg_ms
+            vetor[indice] = a
             indice += 1

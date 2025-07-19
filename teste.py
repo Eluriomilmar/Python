72e8eafc7e7ae8ca027723ebe137dc02f3d4d5dd
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-07-18 22:58:09 -0300

    Pessoais

--- teste.py
+++ teste.py
@@ -1,8 +1,2 @@
-import win32gui
+from math import sqrt
 
-def pos_janela(nome_janela):
-    janela = win32gui.FindWindow(None, nome_janela)
-    return win32gui.GetWindowRect(janela)
-
-a = pos_janela("Velha")
-print(a)

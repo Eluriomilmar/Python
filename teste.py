d517d34ad5579a60efe38e13cf012b3bcfb49820
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-07-20 21:18:40 -0300

    PG Zero

--- teste.py
+++ teste.py
@@ -1,2 +1,14 @@
-from math import sqrt
+from pygame import rect
+import pgzrun
 
+
+WIDTH = 800
+HEIGHT = 600
+
+def draw():
+    screen.clear()
+    screen.draw.line((10, 10),(790, 10),(255, 255, 255))
+    screen.draw.line((), (), ())
+
+
+pgzrun.go()

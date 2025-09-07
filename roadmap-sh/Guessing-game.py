d3b239fbc0b01c74fe1a7c0216b465898de77a25
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-09-06 23:52:18 -0300

    Roadmap.sh

---
+++ roadmap-sh/Guessing-game.py
@@ -0,0 +1,24 @@
+from random import randint
+
+number = randint(0,100)
+guess = int(input("Guess the number(0-100): "))
+min = 0
+max = 100
+total = 1
+if guess < number:
+    min = guess
+else:
+    max = guess
+while guess != number:
+    print(f"Min: {min}\n"
+          f"Max: {max}")
+    total += 1
+    if guess < number:
+        guess = int(input("Guess higher: "))
+    else:
+        guess = int(input("Guess lower: "))
+    if guess < number:
+        min = guess
+    else:
+        max = guess
+print(f"Congratulations, you've guessed the number {number} correctly after {total} tries!")

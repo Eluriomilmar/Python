522bc320636a615a3676a3b041bfdaae6f690675
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-11-14 09:04:29 -0300

    Roadmap.sh

--- roadmap-sh/Test.py
+++ roadmap-sh/Test.py
@@ -1,7 +1,6 @@
-from animal_class import Dog, Cat, Mouse
+import shapes_class
 
-dog = Dog("Scooby")
-cat = Cat("Garfield")
-mouse = Mouse("Mickey")
+shapes = [shapes_class.Circle(4), shapes_class.Square(5), shapes_class.Triangle(6,7), shapes_class.Pizza("Pepperoni", 15)]
 
-print(f"{dog.race}")
+for shape in shapes:
+    print(shape.area())
---
+++ roadmap-sh/duckTyping.py
@@ -0,0 +1,24 @@
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Car:
+
+    alive = False
+
+    def speak(self):
+        print("Honk!")
+
+
+Animals = [Dog(), Cat(), Car()]
+
+for animal in Animals:
+    animal.speak()
+    print(animal.alive)

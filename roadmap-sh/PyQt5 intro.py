e58f86e3e6ab78aab474e3349e15e287bb9c45f6
Eluriomilmar <Lucasrfranca@hotmail.com>
2025-11-15 22:33:42 -0300

    Roadmap.sh

--- roadmap-sh/PyQt5 intro.py
+++ roadmap-sh/PyQt5 intro.py
@@ -2,26 +2,27 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Testing Stuff")
-        self.setGeometry(0, 0, 1600, 900)
+        self.setGeometry(0, 0, 400, 400)
         self.setWindowIcon(QIcon("patchouli.png"))
 
         label = QLabel("IDK TBH man", self)
         label.setFont(QFont("Wingdings", 24))
-        label.setGeometry(0, 0, 1600, 900)
+        label.setGeometry(0, 0, 400, 400)
         label.setStyleSheet("color: #999999;"
                             "background-color: ffffff;"
                             "font-weight: bold;"
                             "font-style: italic;"
                             "text-decoration: underline;")
-
-        label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+        label.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
+        pixmap = QPixmap("patchouli.png")
+        label.setPixmap(pixmap)
 
 
 def main():
